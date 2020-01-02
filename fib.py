4c44ed47b5749c6c0aeb0442bb8319208f239820
anmutu <anmutu@hotmail.com>
2020-01-02 21:46:36 +0800

    递归性能总结和调用次数规律: 递归方式。第35位的数的值是9227465 此次递归耗时4.503948 递归的次数也是一个斐波拉契数列,比如你看:defaultdict(<class 'int'>, {35: 1, 34: 1, 33: 2, 32: 3, 31: 5, 30: 8, 29: 13, 28: 21, 27: 34, 26: 55, 25: 89, 24: 144, 23: 233, 22: 377, 21: 610, 20: 987, 19: 1597, 18: 2584, 17: 4181, 16: 6765, 15: 10946, 14: 17711, 13: 28657, 12: 46368, 11: 75025, 10: 121393, 9: 196418, 8: 317811, 7: 514229, 6: 832040, 5: 1346269, 4: 2178309, 3: 3524578})

--- fib.py
+++ fib.py
@@ -1,8 +1,16 @@
+from collections import defaultdict
+from datetime import datetime
+
+total = defaultdict(int)
+
+
 def fib(k):
     # 求第k个数的数值
     assert k > 0, "k的值必须大于0"
     if k in [1, 2]:
         return 1
+    global total
+    total[k] += 1
     return fib(k - 1) + fib(k - 2)
 
 
@@ -22,6 +30,10 @@ def fib2(k):
 
 
 if __name__ == "__main__":
+    start_time = datetime.now()
+    print("递归方式。第{0}位的数的值是{1}".format(35, fib(35)))
+    print("此次递归耗时{0}".format((datetime.now()-start_time).total_seconds()))
+    print("递归的次数也是一个斐波拉契数列,比如你看:{0}".format(total))
     for i in range(1, 10):
         print("递归方式。第{0}位的数的值是{1}。".format(i, fib(i)))
         print("非递归方式。第{0}位的数的值是{1}。".format(i, fib2(i)))
